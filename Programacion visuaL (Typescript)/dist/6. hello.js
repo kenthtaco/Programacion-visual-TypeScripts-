a406bc8b6d6b27d52d09829c95702463056020ff
kenthtaco <109638218+kenthtaco@users.noreply.github.com>
2023-05-15 21:41:18 -0500

    Add files via upload

---
+++ Programacion visuaL (Typescript)/dist/6. hello.js
@@ -0,0 +1,9 @@
+"use strict";
+var saludo = "Hola Mundo";
+console.log(saludo);
+let contador = 0;
+for (let index = 0; index < 10; index++) {
+    contador = index;
+    console.log(contador);
+    contador = 0;
+}
---
+++ Programacion visuaL (Typescript)/src/2. ejemplos.ts
@@ -0,0 +1,6 @@
+let count = 0 ;
+for (let index = 0; index < 10; index++) {
+    count = index;
+    console.log(count);
+    count = 0;
+}
